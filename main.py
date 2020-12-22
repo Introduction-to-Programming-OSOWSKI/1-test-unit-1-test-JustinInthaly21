0a26dd123f741538d801e137ebb38c422d812447
JustinInthaly21 <825134@mystma.org>
2020-12-22 17:43:48 +0000

    test 1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def name(n):
+    return Justin
+
+def bigAdd(x, y, z, a, b):
+    return x + y + z + a + b
+
+def FourOperations(x, y, z, a, b):
+    return x + y - z * a - b
